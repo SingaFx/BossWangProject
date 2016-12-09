a9652426c68ad61235b6f08066ab3d70522f2478
Zhennan Wang <zhennan.wang@me.com>
2016-12-08 23:19:15 -0800

    update the split Captcha to reduce white margin

--- splitCaptcha.py
+++ splitCaptcha.py
@@ -4,20 +4,35 @@ import os
 
 
 def splitReCaptcha(im, directory):
+    slice_size = 1
+    threshold = 25
     img = cv2.imread(im, 0)
     w = img.shape[1]
-    sp = w/6
-    while not np.all((255 - img[:, sp]) <= 10):
-        sp += 3
-    left = img[:, :sp]
-    right = img[:, sp:]
+    sp = 0
+    start, end = 0, w-1
+    left, right = None, None
+    while sp < w and np.all((255 - img[:, sp]) <= threshold):
+        sp += slice_size
+    start = max(sp - slice_size, start)
+    while sp < w and not np.all((255 - img[:, sp]) <= threshold):
+        sp += slice_size
+    left = sp
+    while sp < w and np.all((255 - img[:, sp]) <= threshold):
+        sp += slice_size
+    right = sp - slice_size
+    while sp < w and not np.all((255 - img[:, sp]) <= threshold):
+        sp += slice_size
+    end = min(end, sp)
+
+    left_img = img[:, start:left]
+    right_img = img[:, right:end]
 
     outdir = directory + '/output/'
 
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    cv2.imwrite(os.path.join(outdir, 'l_' + im), left)
-    cv2.imwrite(os.path.join(outdir, 'r_' + im), right)
+    cv2.imwrite(os.path.join(outdir, 'l_' + im), left_img)
+    cv2.imwrite(os.path.join(outdir, 'r_' + im), right_img)
 
 
 if __name__ == '__main__':
@@ -28,5 +43,5 @@ if __name__ == '__main__':
         if filename.endswith(".jpeg"):
             try:
                 splitReCaptcha(filename, directory)
-            except:
-                print "Error splitting " , filename
+            except Exception as e:
+                print e
