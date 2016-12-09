38d47aaa1c78c61c0e5993f2e66b9e2ada190558
Zhennan Wang <zhennan.wang@me.com>
2016-12-08 23:52:45 -0800

    refactor gen sample to take number as input

--- generateRecaptcha.py
+++ generateRecaptcha.py
@@ -1,36 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-from tqdm import tqdm
 import re
 import os
 import time
+import sys
 
-url = 'https://www.google.com/recaptcha/api/challenge?k=6LfIKgsUAAAAAMTcTP80XeGNN4qzrgluksw030Vf'
-captcha_url = 'https://www.google.com/recaptcha/api/image?c={0}&th={1}'
-NUM = 2000
-for i in range(NUM):
-    try:
-        r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
-        soup = BeautifulSoup(r.content, 'lxml')
-        cregex = re.compile(r"challenge : \'(.*)\'")
-        tregex = re.compile(r"t3 : \'(.*)\'")
 
-        c = cregex.search(str(soup)).group(1)
-        t = tregex.search(str(soup)).group(1)
-        link = captcha_url.format(c, t)
+def getReCaptcha(N):
+    url = 'https://www.google.com/recaptcha/api/challenge?k=6LfIKgsUAAAAAMTcTP80XeGNN4qzrgluksw030Vf'
+    captcha_url = 'https://www.google.com/recaptcha/api/image?c={0}&th={1}'
+    for i in range(N):
+        try:
+            r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
+            soup = BeautifulSoup(r.content, 'lxml')
+            cregex = re.compile(r"challenge : \'(.*)\'")
+            tregex = re.compile(r"t3 : \'(.*)\'")
 
-        r = requests.get(link, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
+            c = cregex.search(str(soup)).group(1)
+            t = tregex.search(str(soup)).group(1)
+            link = captcha_url.format(c, t)
 
-        fname = 'img{0}.jpeg'.format(i)
+            r = requests.get(link, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
 
-        directory = os.getcwd()
-        directory = directory + '/img/'
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        print "Downloading...."
-        with open(directory + fname, "wb") as handle:
-            for data in tqdm(r.iter_content(chunk_size=1024)):
-                handle.write(data)
-        time.sleep(2)
-    except Exception as e:
-        print e
+            fname = 'img{0}.jpeg'.format(i)
+
+            directory = os.getcwd()
+            directory = directory + '/input/'
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            print "Downloading....", fname
+            with open(directory + fname, "wb") as handle:
+                for data in r.iter_content(chunk_size=1024):
+                    handle.write(data)
+            time.sleep(0.5)
+        except Exception as e:
+            print e
+
+if __name__ == '__main__':
+    n = sys.argv[1]
+    print "Generating", n, "recaptchas"
+    getReCaptcha(int(n))
