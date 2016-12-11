de1a84e9bcdfcaf958c6fe47e92ed3d3e06f3588
Zhennan Wang <zhennan.wang@me.com>
2016-12-11 11:31:29 -0800

    separate result verify into functions

--- verify_result.py
+++ verify_result.py
@@ -7,13 +7,12 @@ import socket
 import struct
 
 
-public_key = '6LfIKgsUAAAAAMTcTP80XeGNN4qzrgluksw030Vf'
-private_key = '6LfIKgsUAAAAAHyp-LaOAe2MxaGrdkdokBiX1hSw'
-url = 'https://www.google.com/recaptcha/api/challenge?k={0}'.format(public_key)
-captcha_img_url = 'https://www.google.com/recaptcha/api/image?c={0}&th={1}'
-verify_url = 'https://www.google.com/recaptcha/api/verify'
-NUM = 10
-for i in range(NUM):
+def download_reCaptcha(uid):
+    public_key = '6LfIKgsUAAAAAMTcTP80XeGNN4qzrgluksw030Vf'
+    private_key = '6LfIKgsUAAAAAHyp-LaOAe2MxaGrdkdokBiX1hSw'
+    url = 'https://www.google.com/recaptcha/api/challenge?k={0}'.format(public_key)
+    captcha_img_url = 'https://www.google.com/recaptcha/api/image?c={0}&th={1}'
+
     try:
         # Getting reCAPTCHA image
         r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
@@ -26,7 +25,7 @@ for i in range(NUM):
 
         r = requests.get(link, headers={'User-Agent':'Mozilla/5.0'}, allow_redirects=True)
 
-        fname = 'img{0}.jpeg'.format(i)
+        fname = 'img{0}.jpeg'.format(uid)
 
         directory = os.getcwd()
         directory = directory + '/img/'
@@ -36,17 +35,22 @@ for i in range(NUM):
         with open(directory + fname, "wb") as handle:
             for data in r.iter_content(chunk_size=1024):
                 handle.write(data)
-        time.sleep(2)
 
-        # TODO:
-        # replace the raw_input with our reCAPTCHA break process
-        response = raw_input("Text in reCAPTCHA is ? ")
-        print "text is ", response
+        return directory + fname, challenge
+    except Exception as e:
+        print e
 
 
-        # Verify response
+
+def evaluate(response, challenge):
+    verify_url = 'https://www.google.com/recaptcha/api/verify'
+    # Verify response
+    try:
         fk_ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
         r = requests.post(verify_url, data={'privatekey':private_key, 'response': response, 'challenge': challenge, 'remoteip': fk_ip})
-        print r.text
+        if "true" in r.text:
+            print True
+        else:
+            print False
     except Exception as e:
         print e
